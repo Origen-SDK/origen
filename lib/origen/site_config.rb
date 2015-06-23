f1db8c5acadf975e39a556c91513a33a5d21d07e
Stephen McGinty <stephen.mcginty@freescale.com>
2015-06-23 05:28:23 -0500

    Added missing new files from last commit

---
+++ lib/origen/site_config.rb
@@ -0,0 +1,52 @@
+module Origen
+  class SiteConfig
+    require 'pathname'
+    require 'yaml'
+
+    def method_missing(method, *args, &block)
+      method = method.to_s
+      env = "ORIGEN_#{method.upcase}"
+      if ENV.key?(env)
+        val = ENV[env]
+      else
+        config = configs.find { |c| c.key?(method) }
+        val = config ? config[method] : nil
+      end
+      define_singleton_method(method) do
+        val
+      end
+      val
+    end
+
+    private
+
+    def configs
+      @configs ||= begin
+        path = Pathname.pwd
+        configs = []
+        # Add any site_configs from where we are currently running from, i.e. the application
+        # directory area
+        until path.root?
+          file = File.join(path, 'origen_site_config.yml')
+          configs << YAML.load_file(file) if File.exist?(file)
+          path = path.parent
+        end
+        # Add and any site_configs from the directory hierarchy where Ruby is installed
+        path = Pathname.new($LOAD_PATH.last)
+        until path.root?
+          file = File.join(path, 'origen_site_config.yml')
+          configs << YAML.load_file(file) if File.exist?(file)
+          path = path.parent
+        end
+        # Add the one from the Origen core as the lowest priority, this one defines
+        # the default values
+        configs << YAML.load_file(File.expand_path('../../../origen_site_config.yml', __FILE__))
+        configs
+      end
+    end
+  end
+
+  def self.site_config
+    @site_config ||= SiteConfig.new
+  end
+end
