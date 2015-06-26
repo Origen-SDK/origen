a38f531aeee7b6de1415e8bbdd73a145aab36b88
Stephen McGinty <stephen.f.mcginty@gmail.com>
2015-06-26 10:40:02 +0100

    Update callbacks.rb

--- lib/origen/callbacks.rb
+++ lib/origen/callbacks.rb
@@ -18,7 +18,7 @@ module Origen
   #
   # However within Origen core (and possibly some plugins) it is often the case that registered
   # listeners will be objects that are not re-instantiated upon target load and persist for
-  # the entire Origen thread. In this case use the CoreCallbacks module instead of the regular
+  # the entire Origen thread. In this case use the PersistentCallbacks module instead of the regular
   # Callbacks module to make these objects register as permanent listeners.
   module PersistentCallbacks
     extend ActiveSupport::Concern
