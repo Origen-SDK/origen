5470ec84796e62d6319a8468c6eaad13e32113d6
Stephen McGinty <stephen.mcginty@freescale.com>
2015-07-21 06:06:39 -0500

    Allow template filenames to start with a number

--- lib/origen/generator/compiler.rb
+++ lib/origen/generator/compiler.rb
@@ -182,7 +182,7 @@ module Origen
       # Returns the ERB buffer name for the given file, something like "@my_file_name"
       def buffer_name_for(file)
         # Not sure why the final gsub is needed but seems to fail to parse correctly otherwise.
-        @current_buffer = "@#{file.basename('.*').basename('.*').to_s.gsub('-', '_')}"
+        @current_buffer = "@_#{file.basename('.*').basename('.*').to_s.gsub('-', '_')}"
       end
 
       def merge_file(file, _options = {})
