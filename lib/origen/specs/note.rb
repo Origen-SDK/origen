cb19507a0817bef594a5cd2a8b71a2cff24b3871
David Welguisz <welguisz@freescale.com>
2015-09-16 10:18:39 -0500

    Fixed lint issue.

--- lib/origen/specs/note.rb
+++ lib/origen/specs/note.rb
@@ -2,7 +2,6 @@ module Origen
   module Specs
     # This class is used to store spec note information used to document IP
     class Note
-
       # id is the id for the note.  The goal for the id is to allow multiple specs to reference one note.
       # spec.notes = [id1, id2, id3]
       # spec1.notes = [id1, id4, id5]
@@ -24,7 +23,7 @@ module Origen
       attr_accessor :text
 
       # Markup of the text field.  Currently markup has been tested with
-      # 
+      #
       # * DITA
       # * XML
       # * HTML
