59500a6162fab532115a468f5793b1758cc08fed
Stephen McGinty <stephen.mcginty@freescale.com>
2015-09-21 03:58:52 -0500

    Made 'dut' and 'tester' global methods
    Also removed encodings global methods which should be re-implemented
    elsewhere if required

--- spec/encodings_spec.rb
+++ spec/encodings_spec.rb
@@ -3,14 +3,14 @@ require 'spec_helper'
 
 describe 'Format Encodings' do
   it 'should show users the encoding formats available' do
-    encodings.should == [:utf8]
+    Origen::ENCODINGS.keys.should == [:utf8]
   end
   it 'should show users the keys for a particular format' do
-    encodings(:utf8)[1..3].should == [:cent_sign, :pound_sign, :currency_sign]
+    Origen::ENCODINGS[:utf8].keys[1..3].should == [:cent_sign, :pound_sign, :currency_sign]
   end
-  it 'should show users how to search for an encoded symbol and convert to a String' do
-    encoding_search(/deg/).should == "°"
-    encoding_search(/latin/).first.should == [:latin_capital_letter_a_with_grave, "À"]
-    encoding_search(:latin_capital_letter_eth).should == "Ð"
-  end
+  #it 'should show users how to search for an encoded symbol and convert to a String' do
+  #  encoding_search(/deg/).should == "°"
+  #  encoding_search(/latin/).first.should == [:latin_capital_letter_a_with_grave, "À"]
+  #  encoding_search(:latin_capital_letter_eth).should == "Ð"
+  #end
 end
