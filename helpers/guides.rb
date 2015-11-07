cc6726d28e55aadda5843040128d0dd405fc1f22
Stephen McGinty <stephen.f.mcginty@gmail.com>
2015-11-07 16:52:01 +0000

    Added gitter chat button to website

--- helpers/guides.rb
+++ helpers/guides.rb
@@ -8,6 +8,7 @@ facebook: origensdk
 twitter: origensdk
 author_url: https://plus.google.com/u/1/b/106463981272125989995/106463981272125989 
 site_name: Origen - The Semiconductor Developer's Kit
+gitter_chat: Origen-SDK/users
 END
   end
 
