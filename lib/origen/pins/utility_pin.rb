db90a7a2ac9916ec16ac0d5a71e12cf1a75fd2fe
Burgmeier Aaron-ra4905 <ra4905@lvd0476.am.freescale.net>
2016-02-29 16:55:18 -0600

    Updated Utility pin TYPE restrictions to:  :utility_pin and :ate_ch

--- lib/origen/pins/utility_pin.rb
+++ lib/origen/pins/utility_pin.rb
@@ -1,29 +1,15 @@
 module Origen
   module Pins
     class UtilityPin < Pin
-      # Set the operating voltage for the pin, can be a single value or an array
-      def voltage=(val)
-        @voltages = [val].flatten.uniq
-      end
+      # Pin Types
+      TYPES = [:utility_bit, :ate_ch]
 
-      # Like voltages but if there is only one voltage known then it will be returned
-      # directly instead of being wrapped in an array.
-      # If no voltages are known this returns nil whereas voltages will return an
-      # empty array.
-      # For more than one voltages present this behaves like an alias of voltages.
-      def voltage
-        if voltages.size > 0
-          if voltages.size > 1
-            voltages
-          else
-            voltages.first
-          end
+      def type=(value)
+        if TYPES.include? value
+          @type = value
+        else
+          fail "UtilityPin type '#{value}' must be set to one of the following: #{TYPES.join(', ')}"
         end
-      end
-
-      # Returns an array of known operating voltages for the given pin
-      def voltages
-        @voltages ||= []
       end
     end
   end
