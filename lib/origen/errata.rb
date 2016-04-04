d7f21e49fba6cb6e84720b7502db005f52d9dfc1
Pledger Rebecca-B45586 <Rebecca.Pledger@freescale.com>
2016-04-04 17:09:56 -0500

    Change Errata module to no longer use BaseErratum, add methods sw_workaround and sw_workarounds

--- lib/origen/errata.rb
+++ lib/origen/errata.rb
@@ -2,19 +2,20 @@ module Origen
   module Errata
     autoload :HwErratum,		'origen/errata/hw_erratum'
     autoload :SwErratumWorkaround,	'origen/errata/sw_erratum_workaround'
-    autoload :BaseErratum, 		'origen/errata/base_erratum'
+    #autoload :BaseErratum, 		'origen/errata/base_erratum'
    
     attr_accessor :_errata
     
-    def erratum(id, type, options = {} )
+    def erratum(id, options = {} )
        _errata
-       @_errata[id][type] = BaseErratum.new(id, type, options)
+       #@_errata[id][type] = HwErratum.new(id,type, options)
+       @_errata[id] = HwErratum.new(id, options)
     end
   
     def errata(options = {})
        options = {
-         id:   nil,
-         type: nil
+         id:   nil
+         #type: nil
        }.update(options)
        return nil if @_errata.nil?
        return nil if @_errata.empty?
@@ -24,23 +25,58 @@ module Origen
        end
        
        filter_hash(@_errata, options[:id]).each do |id, hash|
-         filter_hash(hash, options[:type]).each do |type, errata|
-           errata_found[id][type] = errata
-         end
+         #filter_hash(hash, options[:type]).each do |type, errata|
+          # errata_found[id][type] = errata
+        # end
+         errata_found[id] = hash
        end
        if errata_found.empty?
          return nil
        elsif errata_found.size ==1
-         errata_found.values.first.values.first
+         errata_found.values.first #.values.first
        else
          return errata_found
        end
+    end
+
+    def sw_workaround(id, options={})
+      _sw_workarounds
+      @_sw_workarounds[id] = SwErratumWorkaround.new(id, options)
+    end 
+
+    def sw_workarounds(options = {})
+      options = {
+        id: nil
+      }.update(options)
+      return nil if @_sw_workarounds.nil?
+      return nil if @_sw_workarounds.empty?
+
+      sw_workarounds_found = Hash.new do |h, k|
+        h[k] = {}
+      end
+   
+      filter_hash(@_sw_workarounds, options[:id]).each do |id, workarounds|
+        sw_workarounds_found[id] = workarounds
+      end
+      if sw_workarounds_found.empty?
+        return nil
+      elsif sw_workarounds_found.size == 1
+        sw_workarounds_found.values.first.values.first
+      else
+        return sw_workarounds_found
+      end
     end
  
     private
 
     def _errata
       @_errata ||= Hash.new do |h,k|
+        h[k] = {}
+      end
+    end
+ 
+    def _sw_workarounds
+      @_sw_workarounds ||= Hash.new do |h,k|
         h[k] = {}
       end
     end
