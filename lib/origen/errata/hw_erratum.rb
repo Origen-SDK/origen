ac544adbfdf794296df91251d63123f57dfa8559
Pledger Rebecca-B45586 <Rebecca.Pledger@freescale.com>
2016-04-04 17:06:45 -0500

    Change HwErratum Class to no longer extend BaseErratum, all variables except id are options

--- lib/origen/errata/hw_erratum.rb
+++ lib/origen/errata/hw_erratum.rb
@@ -1,15 +1,18 @@
 module Origen
   module Errata
-    class HwErratum < BaseErratum
+    class HwErratum 
 
-      attr_accessor :hw_workaround_description, :disposition, :impact, :fix_plan, :affected_items
-      def initialize(id, title, description, hw_workaround_description, disposition, impact, fix_plan, affected_items, options = {})
-        super(id, type = "hw", title, description)
-        @hw_workaround_description = hw_workaround_description
-        @disposition = disposition
-        @impact = impact
-        @fix_plan = fix_plan
-        @affected_items = affected_items
+      attr_accessor :id, :title, :description, :hw_workaround_description, :disposition, :impact, :fix_plan, :affected_items, :sw_workaround
+      def initialize(id, options = {})
+	@id = id
+	@title = options[:title]
+	@description = options[:description]
+        @hw_workaround_description = options[:hw_workaround_description]
+        @disposition = options[:disposition]
+        @impact = options[:impact]
+        @fix_plan = options[:fix_plan]
+        @affected_items = options[:affected_items]
+	@sw_workaround = options[:sw_workaround]
       end
 
     end
