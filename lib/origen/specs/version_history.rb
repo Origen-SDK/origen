b5d90029b1d52b19f7b8f799e68cc42e78cd01cf
David Welguisz <welguisz@freescale.com>
2016-10-31 09:58:51 -0500

    Added version history requirements for label and internal considerations.

--- lib/origen/specs/version_history.rb
+++ lib/origen/specs/version_history.rb
@@ -2,12 +2,14 @@ module Origen
   module Specs
     # This class is used to store spec exhibit information used to document IP
     class Version_History
-      attr_accessor :date, :author, :changes
+      attr_accessor :label, :date, :author, :changes, :external_changes_internal
 
-      def initialize(date, author, changes)
+      def initialize(date, author, changes, label = nil, external_changes_internal =  nil)
         @date = date
         @author = author
         @changes = changes
+        @label = label
+        @external_changes_internal = external_changes_internal
       end
     end # class Version History
   end # module Specs
