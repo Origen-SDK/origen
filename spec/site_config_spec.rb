dc90f858bd03544badd2cff3a7a89b19f7e07f58
Stephen McGinty <stephen.f.mcginty@gmail.com>
2017-08-23 10:56:29 +0100

    Add additional test case

--- spec/site_config_spec.rb
+++ spec/site_config_spec.rb
@@ -5,10 +5,14 @@ describe "Origen.site_config" do
   # Make sure that cached site config values don't affect these or the
   # next tests
   before :each do
-    Origen.instance_variable_set("@site_config", nil)
+    clear_site_config
   end
 
   after :all do
+    clear_site_config
+  end
+
+  def clear_site_config
     Origen.instance_variable_set("@site_config", nil)
   end
 
@@ -23,6 +27,11 @@ describe "Origen.site_config" do
     with_env_variable("ORIGEN_GEM_MANAGE_BUNDLER", "false") do
       ENV["ORIGEN_GEM_MANAGE_BUNDLER"].should == "false"
       Origen.site_config.gem_manage_bundler.should == false
+    end
+    with_env_variable("ORIGEN_GEM_MANAGE_BUNDLER", "true") do
+      ENV["ORIGEN_GEM_MANAGE_BUNDLER"].should == "true"
+      clear_site_config
+      Origen.site_config.gem_manage_bundler.should == true
     end
   end
 end
