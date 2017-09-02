75c2e319daa89fa34972302c4c10fdba361d4f35
Stephen McGinty <stephen.f.mcginty@gmail.com>
2017-09-02 20:02:14 +0000

    Patch for Ruby 2.4.0 compatibility

--- lib/origen/core_ext/bignum.rb
+++ lib/origen/core_ext/bignum.rb
@@ -1,36 +1,38 @@
-class Bignum
-  # Extend Fixnum to enable 10.cycles
-  def cycles
-    if block_given?
-      times do
-        yield
-        Origen.app.tester.cycle
-      end
-    else
-      Origen.app.tester.cycle(repeat: self)
-    end
-  end
-  alias_method :cycle, :cycles
-
-  alias_method :old_bit_select, :[]
-  def [](*args)
-    if args.length == 1 && !args.first.is_a?(Range)
-      old_bit_select(args.first)
-    else
-      if args.first.is_a?(Range)
-        msb = args.first.first
-        lsb = args.first.last
+unless Gem::Version.new(RUBY_VERSION) >= Gem::Version.new('2.4.0')
+  class Bignum
+    # Extend Fixnum to enable 10.cycles
+    def cycles
+      if block_given?
+        times do
+          yield
+          Origen.app.tester.cycle
+        end
       else
-        msb = args.first
-        lsb = args.last
+        Origen.app.tester.cycle(repeat: self)
       end
-      (self >> lsb) & 0.ones_comp(msb - lsb + 1)
     end
-  end
+    alias_method :cycle, :cycles
 
-  def ones_comp(num_bits)
-    self ^ ((1 << num_bits) - 1)
+    alias_method :old_bit_select, :[]
+    def [](*args)
+      if args.length == 1 && !args.first.is_a?(Range)
+        old_bit_select(args.first)
+      else
+        if args.first.is_a?(Range)
+          msb = args.first.first
+          lsb = args.first.last
+        else
+          msb = args.first
+          lsb = args.last
+        end
+        (self >> lsb) & 0.ones_comp(msb - lsb + 1)
+      end
+    end
+
+    def ones_comp(num_bits)
+      self ^ ((1 << num_bits) - 1)
+    end
+    alias_method :ones_complement, :ones_comp
+    alias_method :ones_compliment, :ones_comp
   end
-  alias_method :ones_complement, :ones_comp
-  alias_method :ones_compliment, :ones_comp
 end
