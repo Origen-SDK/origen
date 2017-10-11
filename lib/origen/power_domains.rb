ab7dc018cdda7174dcb405fa9ad3e9dfb32348bd
Stephen McGinty <stephen.f.mcginty@gmail.com>
2017-10-11 11:31:56 +0100

    Code cleanup

--- lib/origen/power_domains.rb
+++ lib/origen/power_domains.rb
@@ -4,20 +4,20 @@ require_relative './power_domains/power_domains_collection'
 module Origen
   module PowerDomains
     def power_domains(expr = nil)
+      @_power_domains ||= PowerDomainsCollection.new
       if expr.nil?
-        @_power_domains ||= PowerDomainsCollection.new
+        @_power_domains
       else
         @_power_domains.recursive_find_by_key(expr)
       end
     end
 
     def add_power_domain(id, options = {}, &block)
-      @_power_domains ||= PowerDomainsCollection.new
-      if @_power_domains.include?(id)
+      if power_domains.include?(id)
         Origen.log.error("Cannot create power domain '#{id}', it already exists!")
         fail
       end
-      @_power_domains[id] = PowerDomain.new(id, options, &block)
+      power_domains[id] = PowerDomain.new(id, options, &block)
     end
   end
 end
