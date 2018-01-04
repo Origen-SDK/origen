82a88d28404a7af0e1780d8a3258701d10f27351
Engelken Corey-B50956 <B50956@freescale.com>
2018-01-04 10:46:42 -0600

    Completed some automatic setup for workspace paths. Added some dynamic site config methods. Updated docs and specs accordingly.

--- lib/origen/site_config.rb
+++ lib/origen/site_config.rb
@@ -2,6 +2,7 @@ module Origen
   class SiteConfig
     require 'pathname'
     require 'yaml'
+    require 'etc'
 
     TRUE_VALUES = ['true', 'TRUE', '1', 1]
     FALSE_VALUES = ['false', 'FALSE', '0', 0]
@@ -10,13 +11,109 @@ module Origen
 
     # Gets the gem_intall_dir. This is either site_config.home_dir/gems or the site configs gem_install_dir
     def gem_install_dir
-      return "#{tool_repo_install_dir}/gems" if gems_use_tool_repo && tool_repo_install_dir && !user_install_enable
-      find_val('user_gem_dir') || find_val('gem_install_dir') || "#{find_val('home_dir')}/gems"
+      if gems_use_tool_repo && tool_repo_install_dir && !user_install_enable
+        path = eval_path(tool_repo_install_dir)
+      else
+        path = eval_path(find_val('user_gem_dir') || find_val('gem_install_dir') || home_dir)
+      end
+      
+      append = find_val('append_gems')
+      append = 'gems' if (append == true || append.nil?)
+      
+      if append
+        if !path.end_with?(append)
+          path = File.join(path, append)
+        end
+      end
+      path      
     end
+    alias_method :user_gem_dir, :gem_install_dir
 
     # Gets the user_install_dir. Like gem_install_dir, this default to somewhere home_dir, unless overridden
     def user_install_dir
-      find_val('user_install_dir') || find_val('home_dir')
+      eval_path(find_val('user_install_dir') || home_dir)
+    end
+
+    def home_dir
+      eval_path(find_val('home_dir') || '~/')
+    end
+    
+    def eval_path(path, options = {})
+      # Expand the first path. This will take care of replacing any leading ~/ with the home directory.
+      if path.start_with?("\\")
+        path[0] = ''
+      else
+        path = File.expand_path(path)
+      end
+      
+      # Gsub the remaining ~ that aren't escaped.
+      # If it was escaped, eat the escape character
+      path.gsub!(/(?<!\\|\A)~/, "#{Etc.getlogin}")
+      path.gsub!(/\\(?=~)/, '')
+      
+      append = find_val('append_dot_origen')
+      append = '.origen' if (append == true || append.nil?)
+      
+      if append
+        if !path.end_with?(append)
+          path = File.join(path, append)
+        end
+      end
+      path
+    end
+    
+    # Dynamically remove the highest instance of :var
+    def remove_highest(var)
+      @configs.each do |c|
+        if c.key?(var)
+          return c.delete(var)
+        end
+      end
+      
+      # return nil if we haven't returned a value yet
+      nil
+    end
+    
+    # Dynamically remove all the instances of :var
+    def remove_all_instances(var)
+      # Iterate though all the site configs, removing every instance of :var
+      # Return an array containing the value of :var at each config,
+      # from lowest priority to highest.
+      # If [] is returned, it implies that there was no instancs of :var to be removed.
+      ret = Array.new
+      @configs.each do |c|
+        if c.key?(var)
+          ret << c.delete(var)
+        end
+      end
+      ret
+    end
+    alias_method :purge, :remove_all_instances
+    
+    # Dynamically add a new site variable at the highest priority.
+    def add_as_highest(var, value)
+      # Don't want to override anything, so just shift in a dummy site config instance at the highest level and
+      # set the value there.
+      configs.prepend({var.to_s => value})
+    end
+    alias_method :[]=, :add_as_highest
+    
+    # Dynamically add a new site variable at the lowest priority.
+    # Essentially, this sets a new default value.
+    def add_as_lowest(var, value)
+      # Don't want to override anything, so just shift in a dummy site config at the lowest level and
+      # set the value there.
+      configs.append({var.to_s => value})
+    end
+
+    # Adds a new site config file as the highest priority
+    def add_site_config_as_highest(site_config_file)
+      configs.prepend YAML.load_file(File.expand_path('../../../origen_site_config.yml', __FILE__))
+    end
+    
+    # Adds a new site config file as the highest priority
+    def add_site_config_as_lowest(site_config_file)
+      configs.append YAML.load_file(File.expand_path('../../../origen_site_config.yml', __FILE__))
     end
 
     def method_missing(method, *args, &block)
@@ -32,31 +129,61 @@ module Origen
         fail 'Missing site_config value!'
       end
       define_singleton_method(method) do
-        val
+        find_val(method)
       end
       val
     end
 
+    def get(val)
+      find_val(val)
+    end
+    alias_method :[], :get
+    
+    def get_all(val)
+      ret = Array.new
+      @configs.each do |c|
+        if c.key?(val)
+          ret << c[val]
+        end
+      end
+      ret
+    end
+
+    def clear
+      @configs.clear
+    end
+
+    def rebuild!
+      configs!
+    end
+    
     private
 
     def find_val(val, options = {})
       env = "ORIGEN_#{val.upcase}"
       if ENV.key?(env)
-        val = ENV[env]
-        if TRUE_VALUES.include?(val)
-          return true
-        elsif FALSE_VALUES.include?(val)
-          return false
-        end
-        val
+        value = ENV[env]
+        value
       else
         config = configs.find { |c| c.key?(val) }
-        config ? config[val] : nil
+        value = config ? config[val] : nil
       end
+      
+      if TRUE_VALUES.include?(value)
+        return true
+      elsif FALSE_VALUES.include?(value)
+        return false
+      end
+      value
     end
 
     def configs
-      @configs ||= begin
+      @configs ||= configs!
+    end
+    
+    # Forces a reparse of the site configs.
+    def configs!
+      @configs = begin
         # This global is set when Origen is first required, it generally means that what is considered
         # to be the pwd for the purposes of looking for a site_config file is the place from where the
         # user invoked Origen. Otherwise if the running app switches the PWD it can lead to confusing
@@ -96,6 +223,7 @@ module Origen
         configs
       end
     end
+    
   end
 
   def self.site_config
--- spec/site_config_spec.rb
+++ spec/site_config_spec.rb
@@ -1,19 +1,24 @@
 require 'spec_helper'
+require 'etc'
 
-describe "Origen.site_config" do
+fdescribe "Origen.site_config" do
 
   # Make sure that cached site config values don't affect these or the
   # next tests
-  before :each do
-    clear_site_config
-  end
-
-  after :all do
+  before :all do
     clear_site_config
   end
 
   def clear_site_config
-    Origen.instance_variable_set("@site_config", nil)
+    Origen.site_config.instance_variable_set('@configs', Array.new)
+  end
+
+  def username
+    Etc.getlogin
+  end
+  
+  def home
+    File.expand_path '~/'
   end
 
   def with_env_variable(var, value)
@@ -21,6 +26,14 @@ describe "Origen.site_config" do
     ENV[var] = value
     yield
     ENV[var] = orig
+  end
+
+  def add_config_variable(var, value)
+    Origen.site_config.instance_variable_get('@configs').prepend({var => value})
+  end
+  
+  def remove_config_variable(var)
+    Origen.site_config.remove_config_variable(var)
   end
 
   it "converts true/false values from environment variables to booleans" do
@@ -36,13 +49,430 @@ describe "Origen.site_config" do
   end
   
   it "allows user overrides" do
-    with_env_variable("ORIGEN_GEM_INSTALL_DIR", "C:\test\path") do
-      ENV["ORIGEN_GEM_INSTALL_DIR"].should == "C:\test\path"
-      Origen.site_config.gem_install_dir.should == "C:\test\path"
-    end
-    with_env_variable("ORIGEN_USER_INSTALL_DIR", "~/other/path") do
-      ENV["ORIGEN_USER_INSTALL_DIR"].should == "~/other/path"
-      Origen.site_config.user_install_dir.should == "~/other/path"
+    add_config_variable('test', 'test value')
+    Origen.site_config.test.should == 'test value'
+    
+    with_env_variable("ORIGEN_TEST", "Origen Test Value") do
+      ENV["ORIGEN_TEST"].should == "Origen Test Value"
+      Origen.site_config.test.should == "Origen Test Value"
     end
   end
+  
+  describe 'Site config as it relates to install directories' do
+    it 'has method :gem_install_dir (for legacy) aliased to method :user_gem_dir' do
+      expect(Origen.site_config.method(:gem_install_dir)).to eql(Origen.site_config.method(:user_gem_dir))
+    end
+    
+    context 'with default site config' do
+      before :context do
+        clear_site_config
+        
+        # Create a blank site config
+        Origen.site_config.home_dir
+        
+        # Remove the instances of home_dir, user_install_dir, user_gem_dir, and gem_install_dir
+        # that may be present in the user's site configs by default.
+        Origen.site_config.remove_all_instances('home_dir')
+        Origen.site_config.remove_all_instances('user_install_dir')
+        Origen.site_config.remove_all_instances('user_gem_dir')
+        Origen.site_config.remove_all_instances('gem_install_dir')
+      end
+      
+      after :context do
+        clear_site_config
+      end
+      
+      it 'sets home_dir to ~/ by default' do
+        expect(Origen.site_config.home_dir).to eql("#{home}/.origen")
+        expect(Origen.home).to eql("#{home}/.origen")
+      end
+      
+      it 'sets user_install_dir to /home/<username>/.origen by default' do
+        expect(Origen.site_config.user_install_dir).to eql("#{home}/.origen")
+      end
+      
+      it 'sets user_gem_dir to /home/<username>/.origen/gems by default' do
+        expect(Origen.site_config.user_gem_dir).to eql("#{home}/.origen/gems")
+      end
+    end
+    
+    context 'with overriden gem_install_dir' do
+      before :context do
+        add_config_variable('gem_install_dir', '/gem_location/')
+      end
+      
+      after :context do
+        clear_site_config
+      end
+      
+      it 'leaves home_dir as the default' do
+        expect(Origen.site_config.home_dir).to eql("#{home}/.origen")
+        expect(Origen.home).to eql("#{home}/.origen")
+      end
+      
+      it 'leaves the user_install_dir as the default' do
+        expect(Origen.site_config.user_install_dir).to eql("#{home}/.origen")
+      end
+      
+      it 'changes the user_gem_dir' do
+        expect(Origen.site_config.user_gem_dir).to eql("/gem_location/.origen/gems")
+      end
+    end
+    
+    context 'with overriden gem_install_dir AND overriden user_gem_dir' do
+      before :context do
+        add_config_variable('gem_install_dir', '/gem_location/')
+        add_config_variable('user_gem_dir', '/user_location')
+      end
+      
+      after :context do
+        clear_site_config
+      end
+    
+      it 'leaves home_dir as the default' do
+        expect(Origen.site_config.home_dir).to eql("#{home}/.origen")
+        expect(Origen.home).to eql("#{home}/.origen")
+      end
+      
+      it 'leaves the user_install_dir as the default' do
+        expect(Origen.site_config.user_install_dir).to eql("#{home}/.origen")
+      end
+      
+      it 'uses :user_gem_dir over :gem_install_dir' do
+        expect(Origen.site_config.user_gem_dir).to eql("/user_location/.origen/gems")
+      end
+    end
+    
+    context 'with overriden :user_gem_dir AND ENV variable ORIGEN_USER_GEM_DIR set' do
+      before :context do
+        add_config_variable('gem_install_dir', '/gem_location/')
+        add_config_variable('user_gem_dir', '/user_location')
+      end
+      
+      after :context do
+        clear_site_config
+      end
+      
+      it 'uses ORIGEN_USER_GEM_DIR over :user_gem_dir' do
+        with_env_variable 'ORIGEN_USER_GEM_DIR', '/user_env_location' do
+          expect(Origen.site_config.user_gem_dir).to eql("/user_env_location/.origen/gems")
+        end
+      end
+    end
+    
+    context 'with overriden :user_install_dir overrriden' do
+      before :context do
+        clear_site_config
+        
+        add_config_variable('user_install_dir', '/user/install/dir')
+      end
+      
+      it 'moves the :user_install_dir' do
+        expect(Origen.site_config.home_dir).to eql("#{home}/.origen")
+        expect(Origen.home).to eql("#{home}/.origen")
+      end
+      
+      it 'moves the home_dir as well' do
+        expect(Origen.site_config.user_install_dir).to eql('/user/install/dir/.origen')
+      end
+      
+      it 'leaves the :user_gem_install alone' do
+        expect(Origen.site_config.user_gem_dir).to eql("#{home}/.origen/gems")
+      end
+    end
+    
+    context 'with home_dir overriden' do
+      before :context do
+        clear_site_config
+        
+        add_config_variable('home_dir', '/home/dir/')
+      end
+      
+      it 'moves the home_dir' do
+        expect(Origen.site_config.home_dir).to eql("/home/dir/.origen")
+        expect(Origen.home).to eql("/home/dir/.origen")
+      end
+      
+      it 'moves the :user_install_dir as well' do
+        expect(Origen.site_config.user_install_dir).to eql('/home/dir/.origen')
+      end
+      
+      it 'also moves the :user_gem_install' do
+        expect(Origen.site_config.user_gem_dir).to eql('/home/dir/.origen/gems')
+      end
+    end
+    
+    context 'with home_dir, user_gem_dir, and user_install_dir all overriden' do
+      before :context do
+        clear_site_config
+        
+        add_config_variable('home_dir', '/home/location/')
+        add_config_variable('user_install_dir', '/user/install')
+        add_config_variable('gem_install_dir', '/gem/location/')
+        add_config_variable('user_gem_dir', '/user/location')
+      end
+      
+      it 'moves the home_dir' do
+        expect(Origen.site_config.home_dir).to eql("/home/location/.origen")
+        expect(Origen.home).to eql("/home/location/.origen")
+      end
+      
+      it 'moves the :user_install_dir as well' do
+        expect(Origen.site_config.user_install_dir).to eql('/user/install/.origen')
+      end
+      
+      it 'also moves :user_gem_dir' do
+        expect(Origen.site_config.user_gem_dir).to eql('/user/location/.origen/gems')
+      end
+    end
+  end
+  
+  describe 'Site config install directories with ENV variables set' do
+    # Make sure the ENV variables correctly overwrite the site config variables.
+    # These use methods instead, so this moreso making sure the top method works rather than the
+    # underlying find_val.
+    context 'with ORIGEN_GEM_INSTALL_DIR set' do
+      before :context do
+        # Need to clear in the event that :user_gem_dir is actually set
+        clear_site_config
+        
+        ENV['ORIGEN_GEM_INSTALL_DIR'] = '/env/gem/'
+      end
+      
+      it 'uses the value in ORIGEN_GEM_INSTALL_DIR instead of :gem_install_dir' do
+        expect(Origen.site_config.user_gem_dir).to eql("/env/gem/.origen/gems")
+      end
+      
+      it 'uses the value in :user_gem_dir instead of ORIGEN_GEM_INSTALL_DIR since :user_gem_dir takes precedenece' do
+        ENV['ORIGEN_USER_GEM_DIR'] = '/env/user/gem/'
+        expect(Origen.site_config.user_gem_dir).to eql("/env/user/gem/.origen/gems")
+      end
+      
+      after :context do
+        ENV['ORIGEN_GEM_INSTALL_DIR'] = nil
+        ENV['ORIGEN_USER_GEM_DIR'] = nil
+      end
+    end
+    
+    context 'with ORIGEN_HOME_DIR set' do
+      before :context do
+        clear_site_config
+      end
+      
+      it 'uses the value in ORIGEN_HOME_DIR instead of :home_dir' do
+        with_env_variable('ORIGEN_HOME_DIR', '/proj/env/~') do
+          expect(Origen.site_config.home_dir).to eql("/proj/env/#{username}/.origen")
+          expect(Origen.home).to eql("/proj/env/#{username}/.origen")
+          expect(Origen.site_config.user_install_dir).to eql("/proj/env/#{username}/.origen")
+          expect(Origen.site_config.user_gem_dir).to eql("/proj/env/#{username}/.origen/gems")
+        end
+      end
+    end
+    
+    context 'with ORIGEN_HOME_DIR, ORIGEN_USER_INSTALL_DIR, and ORIGEN_USER_GEM_DIR all set' do
+      before :context do
+        clear_site_config
+        
+        ENV['ORIGEN_HOME_DIR'] = '/env/home/~'
+        ENV['ORIGEN_USER_INSTALL_DIR'] = '/env/install/~'
+        ENV['ORIGEN_USER_GEM_DIR'] = '/env/gem/~'
+      end
+      
+      it 'uses the home_dir in ORIGEN_HOME_DIR' do
+        expect(Origen.site_config.home_dir).to eql("/env/home/#{username}/.origen")
+        expect(Origen.home).to eql("/env/home/#{username}/.origen")
+      end
+      
+      it 'uses the user_install_dir in ORIGEN_USER_INSTALL_DIR' do
+        expect(Origen.site_config.user_install_dir).to eql("/env/install/#{username}/.origen")
+      end
+      
+      it 'uses the user_gem_dir in ORIGEN_USER_GEM_DIR' do
+        expect(Origen.site_config.user_gem_dir).to eql("/env/gem/#{username}/.origen/gems")
+      end
+            
+      after :context do
+        ENV['ORIGEN_HOME_DIR'] = nil
+        ENV['ORIGEN_USER_INSTALL_DIR'] = nil
+        ENV['ORIGEN_USER_GEM_DIR'] = nil
+      end
+    end
+  end
+  
+  describe 'Evaluating paths for directories' do
+    context 'with basic values' do
+      it 'uses the path given as is, and appends .origen to it' do
+        expect(Origen.site_config.eval_path('/my/path')).to eql('/my/path/.origen')
+      end
+      
+      it 'does not append .origen if .origen is already provided in the path' do
+        expect(Origen.site_config.eval_path('/my/path/.origen')).to eql('/my/path/.origen')
+      end
+      
+      it 'evaluates the path ~/ (default) to the home directory (or C: for Windows)' do
+        expect(Origen.site_config.eval_path('~/')).to eql("#{home}/.origen")
+      end
+    end
+    
+    context 'using ~ in paths' do
+      it 'replaces ~ with <username> and appending .origen' do
+        expect(Origen.site_config.eval_path('/proj/origen/~')).to eql("/proj/origen/#{username}/.origen")
+      end
+      
+      it 'replace all ~ with <username> and appending .origen' do
+        expect(Origen.site_config.eval_path('/proj/~/origens/~')).to eql("/proj/#{username}/origens/#{username}/.origen")
+      end
+      
+      it 'replaces all but leading ~ with <username> and appending .origen' do
+        expect(Origen.site_config.eval_path('~/origens/~')).to eql("/home/#{username}/origens/#{username}/.origen")
+      end
+      
+      it 'allows ~ to be esacped' do
+        expect(Origen.site_config.eval_path('~/\~/~')).to eql("/home/#{username}/~/#{username}/.origen")
+        #expect(Origen.site_config.eval_path("~/\\~/~")).to eql("/home/#{username}/~/#{username}/.origen")
+      end
+      
+      it 'allows leading ~ to be escaped' do
+        expect(Origen.site_config.eval_path('\~/~')).to eql("~/#{username}/.origen")
+      end
+    end
+    
+    context 'with :append_dot_origen set to values' do
+      before :context do
+        clear_site_config
+        
+        # Only bring in the default site config at the root of Origen.
+        Origen.site_config.add_site_config_as_highest("#{Origen.app.root}/origen_site_config.yml")
+      end
+      
+      it 'is set to true by default' do
+        expect(Origen.site_config.append_dot_origen).to be true
+      end
+      
+      it 'does not append .origen when set to false' do
+        add_config_variable('append_dot_origen', 'false')
+        
+        expect(Origen.site_config.append_dot_origen).to be false
+        expect(Origen.site_config.home_dir).to eql("/home/#{username}")
+      end
+      
+      it 'appends whatever append_dot_origen is when it does not equal true or false (TRUE/FALSE/0/1)' do
+        add_config_variable('append_dot_origen', '.test')
+
+        expect(Origen.site_config.append_dot_origen).to eql('.test')
+        expect(Origen.site_config.home_dir).to eql("/home/#{username}/.test")
+      end
+      
+      it 'still appends /gems to :user_gem_dir' do
+        expect(Origen.site_config.user_gem_dir).to eql("/home/#{username}/.test/gems")
+      end
+    end
+      
+    context 'with :append_gems set to values' do
+      before :context do
+        clear_site_config
+        
+        # Only bring in the default site config at the root of Origen.
+        Origen.site_config.add_site_config_as_highest("#{Origen.app.root}/origen_site_config.yml")
+      end
+      
+      it 'is set to true by default' do
+        expect(Origen.site_config.append_gems).to be true
+      end
+      
+      it 'does not append \'gems\' when set to false' do
+        add_config_variable('append_gems', 'false')
+        
+        expect(Origen.site_config.append_gems).to be false
+        expect(Origen.site_config.user_gem_dir).to eql("/home/#{username}/.origen")
+      end
+      
+      it 'appends whatever append_gems is when it does not equal true or false (TRUE/FALSE/0/1)' do
+        add_config_variable('append_gems', 'user_gems')
+        
+        expect(Origen.site_config.user_gem_dir).to eql("/home/#{username}/.origen/user_gems")
+      end
+    end
+  end
+  
+  describe 'Site Config Dynamic Methods' do
+    context 'with dynamically built site config' do
+      it 'can dynamically add a new site config value as the highest priority' do
+        # Add a new variable
+        Origen.site_config.add_as_highest('new_highest', 'new value')
+        expect(Origen.site_config.new_highest).to eql('new value')
+        
+        # Add it again, making sure that it is the new highest value
+        Origen.site_config.add_as_highest('new_highest', 'newer value')
+        expect(Origen.site_config.new_highest).to eql('newer value')
+      end
+      
+      it 'can dynamically add a new site config value as the lowest priority' do
+        Origen.site_config.add_as_lowest('new_lowest', 'new value')
+        expect(Origen.site_config.new_lowest).to eql('new value')
+        
+        Origen.site_config.add_as_lowest('new_lowest', 'newer value')
+        expect(Origen.site_config.new_lowest).to eql('new value')
+      end
+      
+      it 'can dynamically add a new site config value using index notation' do
+        Origen.site_config['new_index'] = 'new index value'
+        expect(Origen.site_config.new_index).to eql('new index value')
+      end
+      
+      it 'can dynamically add a new highest priority value using index notation' do
+        Origen.site_config['new_index'] = 'newer index value'
+        expect(Origen.site_config.new_index).to eql('newer index value')
+      end
+      
+      it 'can retrieve a site config variable\'s current value' do
+        expect(Origen.site_config.get('new_lowest')).to eql('new value')
+      end
+      
+      it 'can retrieve a site config variable\'s current value, and list of values in order of priority' do
+        expect(Origen.site_config.get_all('new_lowest')).to eql(['new value', 'newer value'])
+      end
+      
+      it 'can retreive a site config variable\'s current value using index notation []' do
+        expect(Origen.site_config['new_index']).to eql('newer index value')
+      end
+      
+      it 'gets nil if the requested variable is not in the site config' do
+        expect(Origen.site_config['unknown_value']).to be_nil
+      end
+      
+      it 'gets an empty array if the requested variable is not in the site config' do
+        expect(Origen.site_config.get_all('unknown_value')).to be_empty
+      end
+      
+      it 'can dynamically remove the highest priority of a config variable' do
+        val = Origen.site_config.remove_highest('new_lowest')
+        expect(Origen.site_config.new_lowest).to eql('newer value')
+        expect(val).to eql('new value')
+      end
+      
+      it 'returns nil if the requested variable is not in the site config' do
+        expect(Origen.site_config.remove_highest('unknown_value')).to be_nil
+      end
+      
+      it 'can dynamically purge a config variable from the site config' do
+        vals = Origen.site_config.purge('new_highest')
+        expect(Origen.site_config.new_highest).to be(nil)
+        expect(vals).to eql(['newer value', 'new value'])
+      end
+      
+      it 'returns an empty array if the requested variable is not in the site config' do
+        expect(Origen.site_config.purge('unknown_value')).to be_empty
+      end
+      
+      it 'has method :purge aliased to :remove_all_instances' do
+        expect(Origen.site_config.method(:purge)).to eql(Origen.site_config.method(:remove_all_instances))
+      end
+      
+      it 'can clear the existing site config' do
+        Origen.site_config.clear
+        expect(Origen.site_config.instance_variable_get('@configs')).to be_empty
+      end
+    end
+  end  
 end
