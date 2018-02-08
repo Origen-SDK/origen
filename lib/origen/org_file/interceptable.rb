65f32b44cc2998c04514dd7c44b2b6d65b03f9dd
Stephen McGinty <stephen.mcginty@nxp.com>
2018-02-08 03:58:47 -0700

    Fix for #218

--- lib/origen/org_file/interceptable.rb
+++ lib/origen/org_file/interceptable.rb
@@ -3,20 +3,6 @@ module Origen
     module Interceptable
       def self.included(base)
         base.extend ClassMethods
-
-        # Defers this executing until the whole class has loaded
-        Origen.after_app_loaded do
-          unless (base.instance_methods - Object.methods).include?(:global_path_to)
-            puts 'When adding the OrgFile::Interceptable module to a class, the class must define an instance method called "global_path_to", like this:'
-            puts
-            puts '  # Must return a string that contains a global path to access the given object,'
-            puts '  # here for example if the object was a pin'
-            puts '  def global_path_to'
-            puts '    "dut.pins(:#{id})"'
-            puts '  end'
-            fail "Incomplete integration of OrgFile::Interceptable in #{base}"
-          end
-        end
       end
 
       module ClassMethods
@@ -25,6 +11,16 @@ module Origen
           i = OrgFile::Interceptor.new(o)
           o.__interceptor__ = i
           i.send(:initialize, *args, &block)
+          unless o.respond_to?(:global_path_to)
+            puts 'When adding the OrgFile::Interceptable module to a class, the class must define an instance method called "global_path_to", like this:'
+            puts
+            puts '  # Must return a string that contains a global path to access the given object,'
+            puts '  # here for example if the object was a pin'
+            puts '  def global_path_to'
+            puts '    "dut.pins(:#{id})"'
+            puts '  end'
+            fail "Incomplete integration of OrgFile::Interceptable in #{o.class}"
+          end
           i
         end
       end
