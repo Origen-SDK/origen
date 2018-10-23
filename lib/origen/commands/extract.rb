89918ab6c020686248001fe560f77dbd8073eb98
Stephen McGinty <stephen.mcginty@nxp.com>
2018-10-23 16:55:11 +0100

    Added error checking and sucess message

--- lib/origen/commands/extract.rb
+++ lib/origen/commands/extract.rb
@@ -18,7 +18,16 @@ end
 
 dirname = Pathname.new(archive).basename('.origen').to_s
 
+if File.exist?(dirname)
+  Origen.log.error "The application directory already exists (#{dirname}), delete it and then try again if you want to overwrite it"
+  exit 1
+end
+
 passed = system "tar -xvzf #{archive}"
+unless passed
+  Origen.log.error 'A problem was encountered extracting the tarball, extraction aborted!'
+  exit 1
+end
 
 Dir.chdir dirname do
   Bundler.with_clean_env do
@@ -26,6 +35,19 @@ Dir.chdir dirname do
     ENV['BUNDLE_PATH'] = File.expand_path(Origen.site_config.gem_install_dir)
     ENV['BUNDLE_BIN'] = File.join(Dir.pwd, 'lbin')
     cmd = "bundle install --gemfile #{ENV['BUNDLE_GEMFILE']} --binstubs #{ENV['BUNDLE_BIN']} --path #{ENV['BUNDLE_PATH']} --local"
-    system cmd
+    passed = system cmd
+    unless passed
+      Origen.log.error 'A problem was encountered installing the gem bundle, extraction aborted!'
+      exit 1
+    end
+
+    Origen.log.info 'Trying to boot the application...'
+
+    passed = system 'origen -v'
+    if passed
+      Origen.log.success 'Your application has been extracted and can boot up'
+    else
+      Origen.log.error 'Something went wrong at the final hurdle, your application has been extracted but cannot boot'
+    end
   end
 end
