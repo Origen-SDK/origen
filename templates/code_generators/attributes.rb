78b4abe72c33f27bbe251cacddc92bcea23c3169
Stephen McGinty <stephen.mcginty@nxp.com>
2018-11-29 09:23:06 +0000

    Fix typo

--- templates/code_generators/attributes.rb
+++ templates/code_generators/attributes.rb
@@ -3,7 +3,7 @@
 # and all of its derivatives (if any).
 # ********************************************************************************
 
-# Accessors will automatically be created for any attributes defined here:
+# Accessors will automatically be created for any attributes defined in this file:
 #
 #   dut.address_width   # => 32
 #   dut.has_feature_x?  # => true
@@ -11,7 +11,7 @@
 # To define an attribute simply declare instance variables and assign their
 # values as shown below.
 # Derivative classes will automatically inherit the values defined here unless
-# the are overridden within their attribute file.
+# they are overridden by a derivative attribute file.
 #
 # @address_width = 32
 # @has_feature_x = true
