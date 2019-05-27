1e184e87a72dec23200d6a2ea1574aee1a0e17af
Stephen McGinty <stephen.mcginty@nxp.com>
2019-05-27 16:07:40 +0100

    Updated gem bundle, final release prep

--- templates/code_generators/attributes.rb
+++ templates/code_generators/attributes.rb
@@ -5,11 +5,6 @@
 <% end -%>
 # ********************************************************************************
 
-# Accessors will automatically be created for any attributes defined in this file:
-#
-#   dut.address_width   # => 32
-#   dut.has_feature_x?  # => true
-#
 # To define an attribute simply declare instance variables and assign their
 # values as shown below.
 # Derivative classes will automatically inherit the values defined here unless
@@ -17,3 +12,9 @@
 #
 # @address_width = 32
 # @has_feature_x = true
+#
+# Accessors will automatically be created for any attributes defined in this file:
+#
+#   dut.address_width   # => 32
+#   dut.has_feature_x   # => true
+#   dut.has_feature_x?  # => true  <attribute>? methods are also added for booleans
