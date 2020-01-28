aebf9551604b7eef2932bd185fa00a958992647f
Priyavadan Kumar <priyavadan.kumar@amd.com>
2020-01-28 16:53:31 -0500

    release note fix, try #1

--- spec/range_spec.rb
+++ spec/range_spec.rb
@@ -10,4 +10,9 @@ describe Range do
     (0..3).to_a.should == [0,1,2,3]
     (3..0).to_a.should == [3,2,1,0]
   end
+
+  it 'sorts by min fist and max second' do
+    ary = [(1..3), (1...3), (5..8), (2..3)]
+    ary.sort.should == [(1...3), (1..3), (2..3), (5..8)]
+  end
 end
