9bb275296bef8cb733658044a364e75bc80c2852
Priyavadan Kumar <priyavadan@gmail.com>
2024-07-18 16:45:24 -0500

    fixing broken pushes

--- config/version.rb
+++ config/version.rb
@@ -1,11 +1,7 @@
 module Origen
   MAJOR = 0
   MINOR = 60
-<<<<<<< Updated upstream
-  BUGFIX = 15
-=======
-  BUGFIX = 16
->>>>>>> Stashed changes
+  BUGFIX = 14
   DEV = nil
   VERSION = [MAJOR, MINOR, BUGFIX].join(".") + (DEV ? ".pre#{DEV}" : '')
 end
