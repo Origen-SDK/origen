cc8f67645a0a7a165a91d40bdd9b7040edffae6d
Priyavadan Kumar <priyavadan@gmail.com>
2024-07-18 16:40:07 -0500

    Updated app version and history

--- config/version.rb
+++ config/version.rb
@@ -1,7 +1,11 @@
 module Origen
   MAJOR = 0
   MINOR = 60
+<<<<<<< Updated upstream
   BUGFIX = 15
+=======
+  BUGFIX = 16
+>>>>>>> Stashed changes
   DEV = nil
   VERSION = [MAJOR, MINOR, BUGFIX].join(".") + (DEV ? ".pre#{DEV}" : '')
 end
